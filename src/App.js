4710d4ed86d54c2deac2cd4daa725a7d3b060b06
Ameena-s <ameenabees26@gmail.com>
2024-12-25 23:28:53 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Weather from "./index1";
+import WeatherApp from "./script";
+
+function App() {
+  return (
+    <div>
+      <Weather />
+      <WeatherApp />
+    </div>
+  );
+}
+
+export default App;
